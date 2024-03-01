e138d42d579905161bf1a0d280b0073496e3333e
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-03-01 12:33:07 +0200

    initial

---
+++ src/components/CardList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Card from './Card.vue';
+
+defineProps({
+  items: Array,
+  isFavorites: Boolean,
+})
+
+const emit = defineEmits(['addToFavorite', 'addToCart']);
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5" v-auto-animate>
+    <Card :key="item.id" v-for="item in items" :image-url="item.imageUrl" :title="item.title" :price="item.price"
+      :isFavorite="item.isFavorite" :id="item.id"
+      :onClickFavorite="isFavorites ? null : () => emit('addToFavorite', item)"
+      :onClickAdd="isFavorites ? null : () => emit('addToCart', item)" :is-added="item.isAdded" />
+  </div>
+</template>
